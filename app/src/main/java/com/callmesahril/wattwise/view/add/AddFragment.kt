9acb1c67dccf409297b07b94c420d1603831ce6a
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2023-12-06 13:07:50 +0700

    Initial commit

---
+++ app/src/main/java/com/callmesahril/wattwise/view/add/AddFragment.kt
@@ -0,0 +1,37 @@
+package com.callmesahril.wattwise.view.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.callmesahril.wattwise.R
+
+
+class AddFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            AddFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/callmesahril/wattwise/view/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.callmesahril.wattwise.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.callmesahril.wattwise.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
